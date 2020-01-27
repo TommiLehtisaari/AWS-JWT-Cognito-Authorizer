6affc8526408aee2c632e04dccbfc996b03e905c
Tommi Lehtisaari <tommi@Tommi-MacBook-Pro.local>
2020-01-27 22:35:55 +0200

    Removing environment variable testing

--- lambdas/publichandler.js
+++ lambdas/publichandler.js
@@ -3,9 +3,6 @@ import { success } from '../libs/response-lib';
 // Public API
 export async function main(event) {
   return success({
-    message: 'Welcome to our Public API!',
-    UserPoolId: process.env.USER_POOL_ID,
-    UserPoolClientId: process.env.USER_POOL_CLIENT_ID,
-    UserPoolRegion: process.env.USER_POOL_REGION
+    message: 'Welcome to our Public API!'
   });
 }
